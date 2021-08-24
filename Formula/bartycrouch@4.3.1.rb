64d6749c676eb720901be9b98edebe22ee9ddc27
Jack Tihon <jack@cambly.com>
2021-08-24 15:40:53 -0700

    create new formula with the current bottles

---
+++ Formula/bartycrouch@4.3.1.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class BartycrouchAT431 < Formula
+  desc "BartyCrouch 4.3.1"
+  homepage "https://github.com/Flinesoft/BartyCrouch"
+  url "https://github.com/Flinesoft/BartyCrouch/archive/refs/tags/4.3.1.tar.gz"
+  sha256 "f781c741a526bc5c4cbb57e4c5a40541ba236b947153e45098af41eca67bae4b"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test bartycrouch@4.3.1`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
